8ec1968994e43eb3f9a2c815dfcaad4eb6bdc728
Adithya-S-Nair <adithyasnair00@gmail.com>
2023-10-25 15:42:30 +0530

    home page done with login

--- src/assets/Pages/Home/home.jsx
+++ src/assets/Pages/Home/home.jsx
@@ -15,6 +15,53 @@ const Home = () => {
   return (
     <div>
       <Navbar />
+      <div className="container mt-5">
+        <div className="d-flex justify-content-center gap-5">
+          <div className="card p-3">
+            <div className="card-body text-center">
+              <h3 className="card-title">Single Event Registrations</h3>
+              <p className="card-text h5">100</p>
+            </div>
+          </div>
+          <div className="card p-3">
+            <div className="card-body text-center">
+              <h3 className="card-title">Single Event Registrations</h3>
+              <p className="card-text h5">100</p>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex gap-5 mt-5 justify-content-center">
+          <button className="btn btn-primary d-flex gap-2 align-items-center" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ width: '1.2em' }}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+            </svg>
+            <span>Single Event</span>
+          </button>
+          <button className="btn btn-primary d-flex gap-2 align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ width: '1.2em' }}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+            </svg>
+            <span>Group Event</span>
+          </button>
+        </div>
+      </div>
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
