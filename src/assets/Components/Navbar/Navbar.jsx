1b81acc250337077fb589ed7e59828fa3ed420b3
paulprince24542 <paulprince2454@gmail.com>
2023-10-25 23:02:33 +0530

    Events Page Added

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -3,62 +3,80 @@ import "./Navbar.css";
 import "./custom.scss";
 
 const Navbar = () => {
+  function logout() {
+    localStorage.removeItem("userid");
+    window.location.reload(false);
+  }
   var navlink = [
     {
       name: "Star Of Samyuktha",
       id: "4efbe19c-12a9-4670-95e7-eb6f1cf8bccc",
+      count: 1,
     },
     {
       name: "30s Reel",
       id: "44195d64-24f6-4d6e-9ae1-1609bcb82915",
+      count: 1,
     },
     {
       name: "Ideathon",
       id: "5f32aa49-a0bb-4ce7-af96-2db5266f4753",
+      count: 4,
     },
     {
       name: "Web Designing",
       id: "5faf4f42-fee6-4c80-bb70-5d4e022d4c7a",
+      count: 2,
     },
     {
       name: "Best Singer",
       id: "72c9450f-c062-4115-85d1-7262474b6d92",
+      count: 1,
     },
     {
       name: "Technical Quiz",
       id: "831868d2-a0ee-4cd9-bcb6-bc352774a7c1",
+      count: 1,
     },
     {
       name: "Spot Dance",
       id: "71061614-199e-40e4-9659-8d57528af0c1",
+      count: 1,
     },
     {
       name: "Treasure Hunt",
       id: "c3e39edf-62a1-473e-9e3c-9ad9e67913f1",
+      count: 4,
     },
     {
       name: "Footbal 3 *3",
       id: "91464529-6176-45b7-9b60-d40b7e9415fc",
+      count: 5,
     },
     {
       name: "Photography",
       id: "410a1bc0-050d-4a04-b99f-3b94c0b0d6b4",
+      count: 1,
     },
     {
       name: "Word Hunt",
       id: "a6cb9012-0705-4053-8bd8-dfc11dee3e60",
+      count: 1,
     },
     {
       name: "Fast Typing",
       id: "f92dbfc2-0838-448e-9ee9-3c81e501fa1f",
+      count: 1,
     },
     {
       name: "Coding UG",
       id: "6fb6e213-31f9-4c39-8bf7-516079a98c1e",
+      count: 1,
     },
     {
       name: "Coding PG",
       id: "ca2b6db4-50a4-4422-b857-c01fe719b9d8",
+      count: 1,
     },
   ];
   return (
@@ -106,11 +124,28 @@ const Navbar = () => {
             ></button>
           </div>
           <div className="offcanvas-body">
+            <a href="/">
+              <div className="sidebar-links">Home</div>
+            </a>
             {navlink.map((navdata) => (
-              <a href={"/events/?eventid=" + navdata.id}>
+              <a
+                href={
+                  "/events/?eventid=" + navdata.id + "&count=" + navdata.count
+                }
+              >
                 <div className="sidebar-links">{navdata.name}</div>
               </a>
             ))}
+            <a href="/">
+              <div
+                className="sidebar-links"
+                onClick={() => {
+                  logout();
+                }}
+              >
+                Logout
+              </div>
+            </a>
 
             {/* <div className="sidebar-links">Ideathon</div>
             <div className="sidebar-links">Web Designing</div>
--- src/assets/Pages/Events/events.jsx
+++ src/assets/Pages/Events/events.jsx
@@ -2,72 +2,217 @@ import React, { useEffect, useState } from "react";
 import "./events.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import axios from "axios";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { URL_endpoint } from "../../../config";
 
 const Events = () => {
+  const Navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("userid")) {
+      null;
+    } else {
+      Navigate("/login");
+    }
+  }, [localStorage.getItem("userid")]);
   const [data, setData] = useState([]);
   const [event, setEventName] = useState("qwdwqd");
+  const [count, setCount] = useState(0);
+  const [status, setStatus] = useState(true);
 
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("eventid"));
 
   async function getList() {
-    var url = `https://samyuktha-admin-api.netlify.app/v1/admin/event/single/?eventid=${searchParams.get(
-      "eventid"
-    )}`;
-    var response = await fetch(url);
-    try {
-      const responseJson = await response.json();
-      console.log(responseJson)
-      // setEventName(responseJson.data[0]);
-      setData(responseJson.data);
-      // console.log(data)
-    } catch (err) {
-      console.log(err);
+    console.log("Fetching Data");
+    var participantCount = searchParams.get("count");
+    var eventid = searchParams.get("eventid");
+    if (participantCount == 1) {
+      try {
+        var API_URL = `${URL_endpoint}/v1/admin/event/single?eventid=${eventid}`;
+        var response = await fetch(API_URL);
+        const responseJson = await response.json();
+        console.log(responseJson);
+        setData(responseJson.data);
+        localStorage.setItem("eventname", responseJson.data[0].eventname);
+        setCount(1);
+        if (responseJson.data.length == 0) {
+          setStatus(false);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      try {
+        var API_URL = `${URL_endpoint}/v1/admin/event/group?eventid=${eventid}`;
+        var response = await fetch(API_URL);
+        const responseJson = await response.json();
+        console.log(responseJson);
+        setData(responseJson.data);
+        localStorage.setItem("eventname", "New");
+        setCount(2);
+        if (responseJson.data.length == 0) {
+          setStatus(false);
+        }
+      } catch (err) {
+        console.log(err);
+      }
     }
   }
   useEffect(() => {
     getList();
   }, []);
   var i = 0;
+
+  async function deleteParticipant(id) {
+    console.log("Deleting");
+    console.log(id);
+    if (count == 1) {
+      const rawResponse = await fetch(
+        `${URL_endpoint}/v1/admin/event/delete/single`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id: id }),
+        }
+      );
+      const content = await rawResponse.json();
+      console.log(content);
+      window.location.reload(false);
+    } else {
+      const rawResponse = await fetch(
+        `${URL_endpoint}/v1/admin/event/delete/group`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id: id }),
+        }
+      );
+      const content = await rawResponse.json();
+      console.log(content);
+      window.location.reload(false);
+    }
+  }
+
   return (
     <>
       <Navbar />
-      <div className="container">
-        <h3 className="mt-5 mb-3">{event} Event</h3>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">id</th>
-              <th scope="col">Participant Name</th>
-              <th scope="col">Participant Email</th>
-              <th scope="col">Participant Phone Number</th>
-              <th scope="col">Course Name</th>
-              <th scope="col">College Name</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((d) => (
+      <div className="container-fluid">
+        <h3 className="mt-5 mb-3">{localStorage.getItem("eventname")} Event</h3>
+        {count == 2 ? (
+          <table className="table table-hover">
+            <thead>
               <tr>
-                <th scope="row">{(i = i + 1)}</th>
-                <td>{d.participantname}</td>
-                <td>{d.participantemail}</td>
-                <td>{d.participantphone}</td>
-                <td>{d.coursename}</td>
-                <td>{d.collegename}</td>
-                <td className="d-flex justify-content-evenly">
-                  <button type="button" className="btn btn-primary">
-                    Change
-                  </button>
-                  <button type="button" className="btn btn-danger">
-                    Delete
-                  </button>
-                </td>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Course</th>
+                <th scope="col">College</th>
+                <th scope="col">Participant 2</th>
+                <th scope="col">Participant 3</th>
+                <th scope="col">Participant 4</th>
+                <th scope="col">Participant 5</th>
+                <th scope="col">Payment Status</th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            {status == true ? (
+              <tbody>
+                {data.map((d) => (
+                  <tr>
+                    <th scope="row">{(i = i + 1)}</th>
+                    <td>{d.participant1name}</td>
+                    <td>{d.participant1email}</td>
+                    <td>{d.participant1phone}</td>
+                    <td>{d.coursename}</td>
+                    <td>{d.collegename}</td>
+                    <td>{d.participant2name || "Null"}</td>
+                    <td>{d.participant3name || "Null"}</td>
+                    <td>{d.participant4name || "Null"}</td>
+                    <td>{d.participant5name || "Null"}</td>
+                    <td>{JSON.stringify(d.paymentpaid)}</td>
+                    <td className="d-flex justify-content-evenly flex-column">
+                      <button type="button" className="mb-2 btn btn-success">
+                        Send Mail
+                      </button>
+                      <button type="button" className="mb-2 btn btn-primary">
+                        Change
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={() => {
+                          deleteParticipant(d.id);
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            ) : (
+              <center>
+                {" "}
+                <h3 className="mt-3">No Records Found</h3>
+              </center>
+            )}
+          </table>
+        ) : (
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">id</th>
+                <th scope="col">Participant Name</th>
+                <th scope="col">Participant Email</th>
+                <th scope="col">Participant Phone Number</th>
+                <th scope="col">Course Name</th>
+                <th scope="col">College Name</th>
+                <th scope="col">Payment Status</th>
+                <th></th>
+              </tr>
+            </thead>
+            {status == true ? (
+              <tbody>
+                {data.map((d) => (
+                  <tr>
+                    <th scope="row">{(i = i + 1)}</th>
+
+                    <td>{d.participantname}</td>
+                    <td>{d.participantemail}</td>
+                    <td>{d.participantphone}</td>
+                    <td>{d.coursename}</td>
+                    <td>{d.collegename}</td>
+                    <td>{JSON.stringify(d.paymentpaid)}</td>
+                    <td className="d-flex justify-content-evenly">
+                      <button type="button" className="btn btn-primary">
+                        Change
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={() => {
+                          deleteParticipant(d.id);
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            ) : (
+              <center>
+                <h3 className="mt-3">No Records Found</h3>
+              </center>
+            )}
+          </table>
+        )}
       </div>
     </>
   );
