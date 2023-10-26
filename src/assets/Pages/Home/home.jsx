315665a1aefbe99e9e3a209191bb77329014fa02
Adithya-S-Nair <adithyasnair00@gmail.com>
2023-10-26 12:40:44 +0530

    forms done

--- src/assets/Pages/Home/home.jsx
+++ src/assets/Pages/Home/home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../Components/Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
 import "./home.css";
 import axios from "axios";;
-import {URL_endpoint} from '../../../config'
+import { URL_endpoint } from '../../../config'
 
 const Home = () => {
   const Navigate = useNavigate();
@@ -14,79 +14,112 @@ const Home = () => {
     participantphone: '',
     college: '',
     course: '',
-    userid: localStorage.getItem('userid')
+    userid: localStorage.getItem('userid'),
+    paymentpaid: true
   });
-
+  const [groupData, setGroupData] = useState({
+    eventid: '',
+    participant1email: '',
+    participant1name: '',
+    participant1phone: '',
+    participant2name: '',
+    participant2phone: '',
+    participant3name: '',
+    participant3phone: '',
+    participant4name: '',
+    participant4phone: '',
+    participant5name: '',
+    participant5phone: '',
+    college: '',
+    course: '',
+    userid: localStorage.getItem('userid'),
+    paymentpaid: true
+  });
   const eventData = [
     {
       eventId: "4efbe19c-12a9-4670-95e7-eb6f1cf8bccc",
       eventName: "Star Of Samyuktha",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "44195d64-24f6-4d6e-9ae1-1609bcb82915",
       eventName: "30s Reel",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "5f32aa49-a0bb-4ce7-af96-2db5266f4753",
       eventName: "Ideathon",
-      eventType: 'group'
+      eventType: 'group',
+      teamStrength: '3'
+
     },
     {
       eventId: "5faf4f42-fee6-4c80-bb70-5d4e022d4c7a",
       eventName: "Web Designing",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "72c9450f-c062-4115-85d1-7262474b6d92",
       eventName: "Best Singer",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "831868d2-a0ee-4cd9-bcb6-bc352774a7c1",
       eventName: "Technical Quiz",
-      eventType: 'group'
+      eventType: 'group',
+      teamStrength: '3'
     },
     {
       eventId: "71061614-199e-40e4-9659-8d57528af0c1",
       eventName: "Spot Dance",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "c3e39edf-62a1-473e-9e3c-9ad9e67913f1",
       eventName: "Treasure Hunt",
-      eventType: 'group'
+      eventType: 'group',
+      teamStrength: '3'
     },
     {
       eventId: "91464529-6176-45b7-9b60-d40b7e9415fc",
       eventName: "Footbal 3 *3",
-      eventType: 'group'
+      eventType: 'group',
+      teamStrength: '4'
     },
     {
       eventId: "410a1bc0-050d-4a04-b99f-3b94c0b0d6b4",
       eventName: "Photography",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "a6cb9012-0705-4053-8bd8-dfc11dee3e60",
       eventName: "Word Hunt",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "f92dbfc2-0838-448e-9ee9-3c81e501fa1f",
       eventName: "Fast Typing",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "6fb6e213-31f9-4c39-8bf7-516079a98c1e",
       eventName: "Coding UG",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
     {
       eventId: "ca2b6db4-50a4-4422-b857-c01fe719b9d8",
       eventName: "Coding PG",
-      eventType: 'single'
+      eventType: 'single',
+      teamStrength: '1'
     },
   ];
 
@@ -104,11 +137,37 @@ const Home = () => {
     });
   };
 
-  const handleSingleReg = (event) => {
+  const handleGroupInputChange = (event) => {
+    const { name, value } = event.target;
+    setGroupData({
+      ...groupData,
+      [name]: value,
+    });
+    console.log(groupData);
+  };
+
+  const handleSingleReg = async (event) => {
     event.preventDefault()
     console.log(formData);
-    // axios.post(`${domain.mainDomain}v1/admin/event/single/participant/add`,)
+    const rawResponse = await fetch("", {
+      method: "POST",
+      body: JSON.stringify(formData)
+    })
+    var res = await rawResponse.json()
+    console.log(res)
   }
+
+  const handleGroupReg = async (event) => {
+    event.preventDefault()
+    const rawResponse = await fetch("", {
+      method: "POST",
+      body: JSON.stringify(groupData)
+    })
+    var res = await rawResponse.json()
+    console.log(res)
+  }
+
+
 
   return (
     <>
@@ -173,16 +232,6 @@ const Home = () => {
                   })}
                 </select>
                 <input
-                  name="participantemail"
-                  className="mb-3 form-control"
-                  type="email"
-                  placeholder="Email"
-                  aria-label="email"
-                  aria-describedby="basic-addon1"
-                  value={formData.participantemail}
-                  onChange={handleInputChange}
-                  required />
-                <input
                   name="participantname"
                   className="mb-3 form-control"
                   type="text"
@@ -190,6 +239,16 @@ const Home = () => {
                   aria-label="Username"
                   aria-describedby="basic-addon1"
                   value={formData.participantname}
+                  onChange={handleInputChange}
+                  required />
+                <input
+                  name="participantemail"
+                  className="mb-3 form-control"
+                  type="email"
+                  placeholder="Email"
+                  aria-label="email"
+                  aria-describedby="basic-addon1"
+                  value={formData.participantemail}
                   onChange={handleInputChange}
                   required />
                 <input
@@ -231,19 +290,207 @@ const Home = () => {
           </div>
         </div >
       </div >
+
+
+
+
+
+
       <div class="modal fade" id="group" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Register for group events</h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+              <form onSubmit={handleGroupReg}>
+                <select
+                  name="eventid"
+                  className="form-select mb-3"
+                  aria-label="Default select example"
+                  value={groupData.eventid}
+                  onChange={handleGroupInputChange}
+                  required>
+                  <option selected>Select an event</option>
+                  {eventData.map((event) => {
+                    if (event.eventType === 'group') {
+                      return (
+                        <option key={event.eventId} value={event.eventId}>
+                          {event.eventName}
+                        </option>
+                      );
+                    } else {
+                      return null;
+                    }
+                  })}
+                </select>
+                <input
+                  name="participant1email"
+                  className="mb-3 form-control"
+                  type="email"
+                  placeholder="Email"
+                  aria-label="email"
+                  aria-describedby="basic-addon1"
+                  value={groupData.participant1email}
+                  onChange={handleGroupInputChange}
+                  required />
+
+                <div className="row">
+                  <div className="col-md-6">
+                    <input
+                      name="participant1name"
+                      className="mb-3 form-control"
+                      type="text"
+                      placeholder="Participant 1 Name"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                      value={groupData.participantname}
+                      onChange={handleGroupInputChange}
+                      required />
+                  </div>
+                  <div className="col-md-6">
+                    <input
+                      name="participant1phone"
+                      className="mb-3 form-control"
+                      type="text"
+                      placeholder="Participant 1 Phone"
+                      aria-label="phonenumber"
+                      aria-describedby="basic-addon1"
+                      value={groupData.participantphone}
+                      onChange={handleGroupInputChange}
+                      required />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <input
+                      name="participant2name"
+                      className="mb-3 form-control"
+                      type="text"
+                      placeholder="Participant 2 Name"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                      value={groupData.participantname}
+                      onChange={handleGroupInputChange}
+                      required />
+                  </div>
+                  <div className="col-md-6">
+                    <input
+                      name="participant2phone"
+                      className="mb-3 form-control"
+                      type="text"
+                      placeholder="Participant 2 Phone"
+                      aria-label="phonenumber"
+                      aria-describedby="basic-addon1"
+                      value={groupData.participantphone}
+                      onChange={handleGroupInputChange}
+                      required />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <input
+                      name="participant3name"
+                      className="mb-3 form-control"
+                      type="text"
+                      placeholder="Participant 3 Name"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                      value={groupData.participantname}
+                      onChange={handleGroupInputChange}
+                      required />
+                  </div>
+                  <div className="col-md-6">
+                    <input
+                      name="participant3phone"
+                      className="mb-3 form-control"
+                      type="text"
+                      placeholder="Participant 3 Phone"
+                      aria-label="phonenumber"
+                      aria-describedby="basic-addon1"
+                      value={groupData.participantphone}
+                      onChange={handleGroupInputChange}
+                      required />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <input
+                      name="participant4name"
+                      className="mb-3 form-control"
+                      type="text"
+                      placeholder="Participant 4 Name"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                      value={groupData.participantname}
+                      onChange={handleGroupInputChange}
+                      required />
+                  </div>
+                  <div className="col-md-6">
+                    <input
+                      name="participant4phone"
+                      className="mb-3 form-control"
+                      type="text"
+                      placeholder="Participant 4 Phone"
+                      aria-label="phonenumber"
+                      aria-describedby="basic-addon1"
+                      value={groupData.participantphone}
+                      onChange={handleGroupInputChange}
+                      required />
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <input
+                        name="participant5name"
+                        className="mb-3 form-control"
+                        type="text"
+                        placeholder="Participant 5 Name"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        value={groupData.participantname}
+                        onChange={handleGroupInputChange}
+                        required />
+                    </div>
+                    <div className="col-md-6">
+                      <input
+                        name="participant5phone"
+                        className="mb-3 form-control"
+                        type="text"
+                        placeholder="Participant 5 Phone"
+                        aria-label="phonenumber"
+                        aria-describedby="basic-addon1"
+                        value={groupData.participantphone}
+                        onChange={handleGroupInputChange}
+                        required />
+                    </div>
+                  </div>
+                  <input
+                    name="college"
+                    className="mb-3 form-control"
+                    type="text"
+                    placeholder="College Name"
+                    aria-label="college"
+                    aria-describedby="basic-addon1"
+                    value={groupData.college}
+                    onChange={handleGroupInputChange}
+                    required />
+                  <input
+                    name="course"
+                    className="mb-3 form-control"
+                    type="text"
+                    placeholder="Course Name"
+                    aria-label="course"
+                    aria-describedby="basic-addon1"
+                    value={groupData.course}
+                    onChange={handleGroupInputChange}
+                    required />
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Register</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
