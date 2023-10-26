c9f011e19d6826ea12845f9998dea69d592b84d7
paulprince24542 <paulprince2454@gmail.com>
2023-10-26 15:27:01 +0530

    Build Updated

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 // export var URL_endpoint = "https://samyuktha-admin-api.netlify.app";
-export var URL_endpoint = "http://localhost:5000";
+// export var URL_endpoint = "http://localhost:5000";
+
+export var URL_endpoint = "http://10.10.167.35:5000";
